b36370056baa5ab93e5d8c9a919cea4dcfb8323d
riedelsolutions <38634824+riedelsolutions@users.noreply.github.com>
2018-11-14 04:58:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var ms = document.getElementById("ms");
+var x = ms.offsetTop;
+document.getElementById("maintainwsp").style.display = "none";
+
+function scroll() {
+  if (window.pageYOffset >= x) {
+    ms.classList.add("sticky");
+    document.getElementById("maintainwsp").style.display = "block";
+  } else {
+    ms.classList.remove("sticky");
+    document.getElementById("maintainwsp").style.display = "none";
+  }
+
+}
+
+
+window.onscroll = function() {
+	scroll();
+};
+
+//add whitespace ons croll
